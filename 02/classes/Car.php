be902fa4b274d8368cf878b5932e228a097a833b
Sergey Eremeev <eremeev87@bk.ru>
2023-04-17 17:06:08 +0800

    Second

---
+++ 02/classes/Car.php
@@ -0,0 +1,17 @@
+<?php
+
+class Car
+{
+    public $color;
+    public $whels = 4;
+    public $speed = 180;
+    public $brand;
+
+    //public $year;
+
+    public $path = __DIR__ . '/yeas/';
+    public $test = 1 + 2;
+
+
+
+}
---
+++ 02/index.php
@@ -0,0 +1,27 @@
+<?php
+error_reporting(-1);
+require_once 'classes/Car.php';
+
+$objCar = new Car();
+$objCar_mazda = new Car();
+
+print_r($objCar);
+
+$objCar->color = 'red';
+$objCar->brand = 'BMW';
+$objCar->speed = 200;
+$objCar->year = 2020; // так не особо нужно делать
+
+
+$objCar_mazda->color = 'gray';
+$objCar_mazda->brand = 'mazda';
+$objCar_mazda->speed = 180;
+
+print_r($objCar);
+print_r($objCar_mazda);
+
+echo 'О моем авто!' . PHP_EOL;
+echo 'Марка: '  . $objCar->brand . PHP_EOL;
+echo 'Количество колес: '  . $objCar->whels . PHP_EOL;
+echo 'Скорость: '  . $objCar->speed . PHP_EOL;
+echo 'Цвет: '  . $objCar->color . PHP_EOL;
