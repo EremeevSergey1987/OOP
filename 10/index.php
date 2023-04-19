9c06af4ada410d668da716f20e2825734b2c9d35
Sergey Eremeev <eremeev87@bk.ru>
2023-04-19 16:05:02 +0800

    Наследование Классов

---
+++ 10/index.php
@@ -0,0 +1,35 @@
+<?php
+require_once 'classes/Product.php';
+require_once 'classes/I3D.php';
+require_once 'classes/IGadget.php';
+require_once 'classes/BookProduct.php';
+require_once 'classes/NoutbookProduct.php';
+
+
+$book = new BookProduct('Колобок', 100, 888);
+$notebook = new NoutbookProduct('DELL', 35000, 'intel');
+
+//var_dump($notebook instanceof IGadget);
+
+
+print_r($book);
+print_r($notebook);
+
+function offerCase(IGadget $product){
+    echo "<p>Предлагаем чехол для гаджета {$product->getName()}</p>";
+}
+
+offerCase($notebook);
+offerCase($book);
+
+class A{};
+class B extends A{};
+class C{};
+
+$a = new A;
+$b = new B;
+$c = new C;
+
+
+
+
