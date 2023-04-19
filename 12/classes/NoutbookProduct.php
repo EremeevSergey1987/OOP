0c9f634349ed6151bfeb355b5fded3aa1d53a25f
Sergey Eremeev <eremeev87@bk.ru>
2023-04-19 16:05:32 +0800

    Наследование Классов

---
+++ 12/classes/NoutbookProduct.php
@@ -0,0 +1,31 @@
+<?php
+
+class NoutbookProduct extends Product implements IGadget
+{
+    public $cpu;
+    public function __construct($name, $price, $cpu)
+    {
+        parent::__construct($name, $price);
+        $this->cpu = $cpu;
+    }
+
+    public function getCase()
+    {
+        // TODO: Implement getCase() method.
+    }
+
+    public function getCpu()
+    {
+        return $this->cpu;
+    }
+
+    public function getProduct()
+    {
+        $out = parent::getProduct();
+        $out .= "Процессор {$this->cpu} <br/>";
+        return $out;
+    }
+    public function addProduct($name, $price)
+    {
+    }
+}
---
+++ 12/classes/Product.php
@@ -0,0 +1,53 @@
+<?php
+abstract class Product
+{
+    private $name;
+    protected $price;
+    const TEST = 10;
+
+//    public $public = "public";
+//    protected $protected = "protected";
+//    private $private = "private";
+
+    protected $discount = 0;
+
+    public function __construct($name, $price)
+    {
+        $this->name = $name;
+        $this->price = $price;
+
+    }
+
+    public function getPrice()
+    {
+        return $this->price - ($this->discount / 100 * $this->price);
+    }
+    public function getName()
+    {
+        return $this->name;
+    }
+    public function getProduct()
+    {
+        return "<hr><b>О товаре</b><br /> 
+        Наиенование: {$this->name} <br /> 
+        
+        ";
+    }
+
+    /**
+     * @return int
+     */
+    public function getDiscount()
+    {
+        return $this->discount;
+    }
+
+    /**
+     * @param int $discount
+     */
+    public function setDiscount($discount)
+    {
+        $this->discount = $discount;
+    }
+    abstract protected function addProduct($name, $price);
+}
